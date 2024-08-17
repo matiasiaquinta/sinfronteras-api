4fe51dafc79f458c97caa5ef3095f43015e6a284
Matias Karim Iaquinta <116685639+matiasiaquinta@users.noreply.github.com>
2024-08-17 12:57:37 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,9 +6,8 @@ export const FRONTEND_URL =
     process.env.FRONTEND_URL || "https://sinfronteras.onrender.com";
 
 export const PORT = process.env.PORT || 4000;
-//export const MONGODB_URI =
-//    process.env.MONGODB_URI ||
-//    "mongodb+srv://sinfronteras:3FlkqeWld1irBtC7@sinfronteras.5lqm1.mongodb.net/sinfronteras?retryWrites=true&w=majority&appName=sinfronteras";
+export const MONGODB_URI =
+    process.env.MONGODB_URI
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 //export const TOKEN_SECRET = "some secret key";
