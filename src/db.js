9647d7ec5c40a70782da14aa8725cc64c46d3a85
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-17 14:54:56 -0300

    test05

--- src/db.js
+++ src/db.js
@@ -1,12 +1,16 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import { MONGODB_URI } from "./config.js";
 
-mongoose
-    .connect(
-        "mongodb+srv://matias:E41XAmNjkgG4bjZR@sinfronteras-api.5lqm1.mongodb.net/sinfronteras-api"
-    )
-    .then(() => {
+export const connectDB = async () => {
+    try {
+        // Conectar a MongoDB usando Mongoose
+        await mongoose.connect(MONGODB_URI);
+        console.log("MongoDB is connected on", MONGODB_URI);
+        console.log("PORT ", PORT);
+
+        await mongoose.connect(MONGODB_URI);
         console.log("MongoDB is connected");
-    })
-    .catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
-    });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+    }
+};
