5756ac3c6d21913b0e7e7b5b99504ff3fe179d70
Matias Karim Iaquinta <116685639+matiasiaquinta@users.noreply.github.com>
2024-08-17 14:03:03 -0300

    test02

--- src/db.js
+++ src/db.js
@@ -4,10 +4,7 @@ import { MONGODB_URI } from "./config.js";
 export const connectDB = async () => {
     try {
         // Conectar a MongoDB usando Mongoose
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(MONGODB_URI);
         console.log("MongoDB is connected");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
