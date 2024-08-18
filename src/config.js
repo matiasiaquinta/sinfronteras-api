18b13b4b968de8b1090bf2586b07d49f5e70b4f1
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-18 13:03:16 -0300

    updating files and urls

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export const FRONTEND_URL =
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =
     process.env.MONGODB_URI ||
-    "mongodb+srv://matias:E41XAmNjkgG4bjZR@sinfronteras-api.5lqm1.mongodb.net/sinfronteras-api?retryWrites=true&w=majority&appName=sinfronteras-api";
+    "mongodb+srv://sinfronteras:3FlkqeWld1irBtC7@cluster0.5lqm1.mongodb.net/sinfronteras-api?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
