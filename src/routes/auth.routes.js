5082274c1cac5d681b249128e76540a887acfe6f
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-26 21:57:40 -0300

    test without validateSchema

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,7 +11,7 @@ import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
-router.post("/login", validateSchema(loginSchema), login);
+router.post("/login", login);
 router.post("/logout", logout);
 router.get("/verify", verifyToken);
 
