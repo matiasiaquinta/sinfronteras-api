1e60592c571319c4c6f15ff0ed10927cf70fa85f
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-26 21:17:05 -0300

    token original

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -67,13 +67,13 @@ export const login = async (req, res) => {
         const token = await createAccessToken({ id: userFound._id });
 
         // Configura la cookie
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production", // Solo en HTTPS en producción
-            sameSite: "strict", // Necesario para cookies en diferentes dominios
-        });
+        //res.cookie("token", token, {
+        //    httpOnly: true,
+        //    secure: process.env.NODE_ENV === "production", // Solo en HTTPS en producción
+        //    sameSite: "strict", // Necesario para cookies en diferentes dominios
+        //});
 
-        //res.cookie("token", token);
+        res.cookie("token", token);
 
         res.json({
             id: userFound._id,
